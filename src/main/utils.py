62e8d37f95bfac9f220735fdf288b5e66d7fb6e2
TimurGizatullin <timur.gizatullin@scalewill.com>
2023-11-10 08:10:46 -0800

    ! fix check censor

--- src/main/utils.py
+++ src/main/utils.py
@@ -16,7 +16,6 @@ user_uri = "admin/main/user/{}/change/"
 
 async def is_has_censor(message: str, censor_list: list[str]) -> bool:
     message = message.lower()
-    message = message.replace(" ", "")
 
     for censor_word in censor_list:
         if message.find(censor_word.lower()) != -1:
